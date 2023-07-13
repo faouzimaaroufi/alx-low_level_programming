8ed86a3bb3449cb5e0fdce96ff586b267d108caf
faouzi maaroufi <Faouzi Maaroufi>
2023-07-13 06:39:01 -0700

    fdfd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"\n);
-	return (0);
+        puts("with proper grammar, but the outcome is a piece of art,");
+        return (0);
 }
